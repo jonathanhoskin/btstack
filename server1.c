3ef6e8de0fc09e2ddceb79a426d415fda5b99a00
Jono Hoskin <github@hosk.in>
2024-10-31 13:55:36 +1300

    WIP

---
+++ server1.c
@@ -0,0 +1,50 @@
+// This is from http://www.microhowto.info/howto/listen_for_and_receive_udp_datagrams_in_c.html
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/uio.h>
+
+
+const char* hostname=0; /* wildcard */
+const char* portname="daytime";
+
+struct addrinfo hints;
+
+memset(&hints,0,sizeof(hints));
+
+hints.ai_family = AF_UNSPEC;
+hints.ai_socktype = SOCK_DGRAM;
+hints.ai_protocol = 0;
+hints.ai_flags = AI_PASSIVE|AI_ADDRCONFIG;
+
+struct addrinfo* res = 0;
+
+// int err = getaddrinfo(hostname,portname,&hints,&res);
+// if (err != 0) {
+//   die("failed to resolve local socket address (err = %d)", err);
+// }
+
+int fd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+if (fd == -1) {
+  die("%s",strerror(errno));
+}
+
+int bind = bind(fd,res->ai_addr,res->ai_addrlen);
+if (bin == -1) {
+  die("%s",strerror(errno));
+}
+
+char buffer[549];
+struct sockaddr_storage src_addr;
+socklen_t src_addr_len=sizeof(src_addr);
+ssize_t count=recvfrom(fd,buffer,sizeof(buffer),0,(struct sockaddr*)&src_addr,&src_addr_len);
+if (count==-1) {
+  die("%s",strerror(errno));
+} else if (count==sizeof(buffer)) {
+  warn("datagram too large for buffer: truncated");
+} else {
+  handle_datagram(buffer,count);
+}
